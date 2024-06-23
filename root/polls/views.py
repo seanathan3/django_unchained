6cfe8198c3be2d5fe01134eaf720d9f1f751207c
Sean Abbas <sean.abbas33@gmail.com>
2024-06-23 14:23:59 -0400

    configure model tests

--- root/polls/views.py
+++ root/polls/views.py
@@ -12,7 +12,7 @@ from .models import Question, Choice
 
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
-    context_object_name = "lastest_question_list"
+    context_object_name = "latest_question_list"
 
     def get_queryset(self):
         return Question.objects.order_by("-pub_date")[:5]
